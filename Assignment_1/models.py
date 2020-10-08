89568a305cb29e449ed83562def2813e4ebce0ae
nguyen <nguyennguyen8251@gmail.com>
2020-10-08 17:46:31 -0400

    add evaluation

--- Assignment_1/models.py
+++ Assignment_1/models.py
@@ -10,11 +10,11 @@ import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import sklearn
-import sklearn.preprocessing    # For scale function
-import sklearn.metrics          # For accuracy_score
+import sklearn.preprocessing         # For scale function
+import sklearn.metrics as metrics    # Fo# for label size
 
 
-# In[115]:
+# In[21]:
 
 
 #load
@@ -32,7 +32,7 @@ info1 = pd.read_csv('Assig1-Dataset/info_1.csv')
 info2 = pd.read_csv('Assig1-Dataset/info_2.csv')
 
 
-# In[ ]:
+# In[20]:
 
 
 #normalize
@@ -44,64 +44,68 @@ X_train = sc.fit_transform(x_train)
 X_test = sc.transform(x_test)
 
 
-# In[111]:
+# In[81]:
 
 
 #GaussianNB
 from sklearn.naive_bayes import GaussianNB
-model = GaussianNB()
-validation(1)
-validation(2)
+model1 = GaussianNB()
+model2 = GaussianNB()
+process("GNB")
 
 
-# In[112]:
+# In[34]:
 
 
 #Baseline Decision Tree
 from sklearn.tree import DecisionTreeClassifier
-model = DecisionTreeClassifier(criterion="entropy")
-validation(1)
-validation(2)
+model1 = DecisionTreeClassifier(criterion="entropy")
+model2 = DecisionTreeClassifier(criterion="entropy")
+process("Base-DT")
 
 
-# In[113]:
+# In[28]:
 
 
 #Best Decision Tree
 from sklearn.tree import DecisionTreeClassifier
-model = DecisionTreeClassifier(criterion="gini", max_depth=None, min_samples_split=2, min_impurity_decrease=0.0, class_weight="balanced")
-validation(1)
-validation(2)
+model1 = DecisionTreeClassifier(criterion="gini", max_depth=None, min_samples_split=2, min_impurity_decrease=0.0, class_weight="balanced")
+model2 = DecisionTreeClassifier(criterion="gini", max_depth=None, min_samples_split=2, min_impurity_decrease=0.0, class_weight="balanced")
+process("Best-DT")
 
 
-# In[136]:
+# In[35]:
 
 
 #Perceptron
 from sklearn.linear_model import Perceptron
-model = Perceptron()
-validation(1)
+model1 = Perceptron()
+model2 = Perceptron()
+process("PER")
 
 
-# In[101]:
+# In[82]:
 
 
 #Base MLP
 from sklearn.neural_network import MLPClassifier
-model = MLPClassifier(hidden_layer_sizes=(100), activation='logistic', solver='sgd')
-validation(1)
+model1 = MLPClassifier(hidden_layer_sizes=(100), activation='logistic', solver='sgd')
+model2 = MLPClassifier(hidden_layer_sizes=(100), activation='logistic', solver='sgd')
+process("Base-MLP")
 
 
-# In[135]:
+# In[84]:
 
 
 #Best MLP
+#model = MLPClassifier(hidden_layer_sizes=(150,100,50), activation='relu', learning_rate_init=0.01, max_iter=500,  solver='adam', random_state=1)
 from sklearn.neural_network import MLPClassifier
-model = MLPClassifier(hidden_layer_sizes=(150,100,50), activation='relu', learning_rate_init=0.01, max_iter=500,  solver='adam', random_state=1)
-validation(1)
+model1 = MLPClassifier(hidden_layer_sizes=(150,100,50), activation='relu', solver='adam', random_state=1)
+model2 = MLPClassifier(hidden_layer_sizes=(150,100,50), activation='relu', solver='adam', random_state=1)
+process("Best-MLP")
 
 
-# In[109]:
+# In[64]:
 
 
 #validation
@@ -111,20 +115,22 @@ def validation(index):
     if index==1:
         X_train = data1.values[:,:-1]
         Y_train = data1.values[:,-1:]
-        model.fit(X_train, Y_train)
+        model1.fit(X_train, Y_train)
         X_val = vali1.values[:,:-1]
         Y_val = vali1.values[:,-1:]
+        Y_pred = model1.predict(X_val)
     elif index==2:
         X_train = data2.values[:,:-1]
         Y_train = data2.values[:,-1:]
-        model.fit(X_train, Y_train)
+        model2.fit(X_train, Y_train)
         X_val = vali2.values[:,:-1]
         Y_val = vali2.values[:,-1:]
-    predict_test = model.predict(X_val)
-    print(metrics.accuracy_score(Y_val, predict_test))
+        Y_pred = model2.predict(X_val)
+
+    print(metrics.accuracy_score(Y_val, Y_pred))
 
 
-# In[110]:
+# In[60]:
 
 
 #test
@@ -132,53 +138,85 @@ def test(index):
     """
     """
     if index==1:
-        X_val = test1.values[:,:-1]
-        Y_val = test1.values[:,-1:]
+        X_test = test1.values[:,:-1]
+        Y_test = test1.values[:,-1:]
+        Y_pred = model1.predict(X_test)
     elif index==2:
-        X_val = test2.values[:,:-1]
-        Y_val = test2.values[:,-1:]
-    predict_test = model.predict(X_val)
-    print(metrics.accuracy_score(Y_val, predict_test))
+        X_test = test2.values[:,:-1]
+        Y_test = test2.values[:,-1:]
+        Y_pred = model2.predict(X_test)
+    
+    print(metrics.accuracy_score(Y_test, Y_pred))
 
 
-# In[137]:
+# In[47]:
 
 
-#test1
-X_val = test1.values[:,:-1]
-Y_val = test1.values[:,-1:]
-predict_test = model.predict(X_val)
-print(metrics.accuracy_score(Y_val, predict_test))
+#save
+def save(name, index):
+    """
+    """
+    if index==1:
+        Input = samp1.values
+        Output = model1.predict(Input)
+    elif index==2:
+        Input = samp2.values
+        Output = model2.predict(Input)
+    
+    dict = {'Output': Output}  
+    df = pd.DataFrame(dict) 
+    df.to_csv(name + "-DS" + str(index) + ".csv") 
 
 
-# In[ ]:
+# In[80]:
 
 
-#test2
-X_val = test2.values[:,:-1]
-Y_val = test2.values[:,-1:]
-predict_test = model.predict(X_val)
-print(metrics.accuracy_score(Y_val, predict_test))
+#evaluation
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+import seaborn as sn
+def evaluation(index):
+    """
+    """
+    if index==1:
+        X_val = vali1.values[:,:-1]
+        Y_val = vali1.values[:,-1:]
+        Y_pred = model1.predict(X_val)
+    elif index==2:
+        X_val = vali2.values[:,:-1]
+        Y_val = vali2.values[:,-1:]
+        Y_pred = model2.predict(X_val)
+
+    confusion = confusion_matrix(Y_val, Y_pred)
+    print('Confusion Matrix\n')
+    plt.figure(figsize = (20,20))
+    sn.set(font_scale=1) # for label size
+    sn.heatmap(confusion, annot=True, annot_kws={"size": 12}, cmap='Oranges', fmt='d') # font size
+    plt.show()
+
+    print('\nClassification Report\n')
+    print(classification_report(Y_val, Y_pred))
 
 
-# In[138]:
+# In[83]:
 
 
-#save1
-Input = samp1.values
-Output = model.predict(Input)
-dict = {'Output': Output}  
-df = pd.DataFrame(dict) 
-df.to_csv('submission_1.csv') 
+#process
+def process(name):
+    """
+    """
+    validation(1)
+    test(1)
+    save(name, 1) 
+    validation(2)
+    test(2)
+    save(name, 2)
+    evaluation(1)
+    evaluation(2)
 
 
-# In[134]:
+# In[56]:
 
 
-#save2
-Input = samp2.values
-Output = model.predict(Input)
-dict = {'Output': Output}  
-df = pd.DataFrame(dict) 
-df.to_csv('submission_2.csv') 
+
 
