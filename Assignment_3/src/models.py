7ee9ef5742a30170ccc3d04b803baf53abfe1e9c
nguyen <nguyennguyen8251@gmail.com>
2020-12-07 15:51:54 -0500

    filter version

--- Assignment_3/src/models.py
+++ Assignment_3/src/models.py
@@ -54,51 +54,55 @@ class TweetDataset(torch.utils.data.Dataset):
         return tweet, annotation, tweet_id
 
 
-class NaiveBayes:
-    def __init__(self,unique_classes):
+class NB_BOW:
+    def __init__(self, unique_classes, filter = False):
         # Constructor is sinply passed with unique number of classes of the training set
-        self.classes=unique_classes 
-        
+        self.classes = unique_classes
+        if(filter == False):
+            self.id = "OV"
+        else:
+            self.id = "FV"
 
-    def addToBow(self,document,dict_index):
+    def addToBow(self, document, dict_index):
         '''
             dict_index = class_index
-        '''  
-        if isinstance(document,np.ndarray): document=document[0]
-     
-        for token_word in document.split():          
-            self.bow_dicts[dict_index][token_word]+=1
-    
-            
-    def train(self,dataset,labels):
+        '''
+        if isinstance(document, np.ndarray):
+            document = document[0]
+
+        for token_word in document.split():
+            self.bow_dicts[dict_index][token_word] += 1
+
+    def train(self, dataset, labels):
         '''
             Parameters:
             1. dataset - shape = (m X d)
             2. labels - shape = (m,)        
         '''
-        self.docs=dataset
-        self.labels=labels
-        self.bow_dicts=np.array([defaultdict(lambda:0) for index in range(self.classes.shape[0])])
-        
+        self.docs = dataset
+        self.labels = labels
+        self.bow_dicts = np.array([defaultdict(lambda:0)
+                                   for index in range(self.classes.shape[0])])
+
         #only convert to numpy arrays if initially not passed as numpy arrays - else its a useless recomputation
-        if not isinstance(self.docs,np.ndarray): 
-            self.docs=np.array(self.docs)
-        if not isinstance(self.labels,np.ndarray): 
-            self.labels=np.array(self.labels)
-            
+        if not isinstance(self.docs, np.ndarray):
+            self.docs = np.array(self.docs)
+        if not isinstance(self.labels, np.ndarray):
+            self.labels = np.array(self.labels)
+
         #constructing BoW for each category
         for index, cat in enumerate(self.classes):
             #filter all docs of category == cat
-            all_cat_docs = self.docs[self.labels==cat] 
-            
+            all_cat_docs = self.docs[self.labels == cat]
+
             #get docs preprocessed
-            cleaned_docs=[util.preProcess(cat_example) for cat_example in all_cat_docs]
-            cleaned_docs=pd.DataFrame(data=cleaned_docs)
-            
+            cleaned_docs = [util.preProcess(cat_example)
+                            for cat_example in all_cat_docs]
+            cleaned_docs = pd.DataFrame(data=cleaned_docs)
+
             #now costruct BoW of this particular category
-            np.apply_along_axis(self.addToBow,1,cleaned_docs,index)
-            
-        
+            np.apply_along_axis(self.addToBow, 1, cleaned_docs, index)
+
         '''
             ------------------------------------------------------------------------------------
             Test Time Forumla: {for each word w [ count(w|c)+1 ] / [ count(c) + |V| + 1 ] } * p(c)
@@ -111,32 +115,34 @@ class NaiveBayes:
             3. denominator value of each class - [ count(c) + |V| + 1 ] 
             
         '''
-    
+
         smoothing = 0.01
-        prob_classes=np.empty(self.classes.shape[0])
-        all_words=[]
-        cat_word_counts=np.empty(self.classes.shape[0])
-        for index,cat in enumerate(self.classes):
-           
+        prob_classes = np.empty(self.classes.shape[0])
+        all_words = []
+        cat_word_counts = np.empty(self.classes.shape[0])
+        for index, cat in enumerate(self.classes):
+
             #Calculating prior probability p(c) for each class
-            prob_classes[index]=np.sum(self.labels==cat)/float(self.labels.shape[0]) 
-            
-            #Calculating total counts of all the words of each class 
-            count=list(self.bow_dicts[index].values())
-            cat_word_counts[index]=np.sum(np.array(list(self.bow_dicts[index].values()))) + smoothing # |v| is remaining to be added
-            
-            #get all words of this category                                
-            all_words+=self.bow_dicts[index].keys()
-                                                     
-        
+            prob_classes[index] = np.sum(
+                self.labels == cat)/float(self.labels.shape[0])
+
+            #Calculating total counts of all the words of each class
+            count = list(self.bow_dicts[index].values())
+            cat_word_counts[index] = np.sum(np.array(list(
+                self.bow_dicts[index].values()))) + smoothing  # |v| is remaining to be added
+
+            #get all words of this category
+            all_words += self.bow_dicts[index].keys()
+
         #combine all words of every category & make them unique to get vocabulary -V- of entire training set
-        
-        self.vocab=np.unique(np.array(all_words))
-        self.vocab_length=self.vocab.shape[0]
-                                  
-        #computing denominator value                                      
-        denoms=np.array([cat_word_counts[index]+self.vocab_length + smoothing for index,cat in enumerate(self.classes)])                                                                          
-      
+
+        self.vocab = np.unique(np.array(all_words))
+        self.vocab_length = self.vocab.shape[0]
+
+        #computing denominator value
+        denoms = np.array([cat_word_counts[index]+self.vocab_length +
+                           smoothing for index, cat in enumerate(self.classes)])
+
         '''
             Now that we have everything precomputed as well, its better to organize everything in a tuple 
             rather than to have a separate list for every thing.
@@ -144,45 +150,47 @@ class NaiveBayes:
             Every element of self.cats_info has a tuple of values
             Each tuple has a dict at index 0, prior probability at index 1, denominator value at index 2
         '''
-        
-        self.cats_info=[(self.bow_dicts[index],prob_classes[index],denoms[index]) for index,cat in enumerate(self.classes)]                               
-        self.cats_info=np.array(self.cats_info)                                 
-                                              
-                                              
-    def getDocProb(self,test_doc):                                
-        
+
+        self.cats_info = [(self.bow_dicts[index], prob_classes[index], denoms[index])
+                          for index, cat in enumerate(self.classes)]
+        self.cats_info = np.array(self.cats_info)
+
+    def getDocProb(self, test_doc):
         '''
             Probability of test example in ALL CLASSES
-        '''                                      
-                                              
-        likelihood_prob=np.zeros(self.classes.shape[0]) #to store probability w.r.t each class
-        
+        '''
+
+        # to store probability w.r.t each class
+        likelihood_prob = np.zeros(self.classes.shape[0])
+
         #finding probability w.r.t each class of the given test example
-        for index,cat in enumerate(self.classes): 
-            #for each word w [ count(w|c)+1 ] / [ count(c) + |V| + 1 ]      
-            #split the test example and get p of each test word               
-            for test_token in test_doc.split(): 
-                #get total count of this test token from it's respective training dict to get numerator value                           
-                test_token_counts=self.cats_info[index][0].get(test_token,0)+1
-                
-                #now get likelihood of this test_token word                              
-                test_token_prob=test_token_counts/float(self.cats_info[index][2])                              
-                likelihood_prob[index]+=np.log(test_token_prob)
-                                              
+        for index, cat in enumerate(self.classes):
+            #for each word w [ count(w|c)+1 ] / [ count(c) + |V| + 1 ]
+            #split the test example and get p of each test word
+            for test_token in test_doc.split():
+                #get total count of this test token from it's respective training dict to get numerator value
+                test_token_counts = self.cats_info[index][0].get(
+                    test_token, 0)+1
+
+                #now get likelihood of this test_token word
+                test_token_prob = test_token_counts / \
+                    float(self.cats_info[index][2])
+                likelihood_prob[index] += np.log(test_token_prob)
+
         # we have likelihood estimate of the given example against every class but we need posterior probility
-        post_prob=np.empty(self.classes.shape[0])
-        for index,cat in enumerate(self.classes):
-            post_prob[index]=likelihood_prob[index]+np.log(self.cats_info[index][1])                                  
-      
+        post_prob = np.empty(self.classes.shape[0])
+        for index, cat in enumerate(self.classes):
+            post_prob[index] = likelihood_prob[index] + \
+                np.log(self.cats_info[index][1])
+
         return post_prob
-    
-   
-    def test(self,test_set):
+
+    def test(self, test_set):
         '''
             Determines probability of each test example against all classes and predicts the label
             against which the class probability is maximum
         '''
-       
+
         predictions = []  # to store prediction of each test example
         for doc in test_set:
             #preprocess the test example the same way we did for training set exampels
@@ -196,4 +204,3 @@ class NaiveBayes:
             predictions.append(self.classes[np.argmax(post_prob)])
 
         return np.array(predictions)
-
--- Assignment_3/src/utils.py
+++ Assignment_3/src/utils.py
@@ -41,3 +41,67 @@ def preProcess(str_arg):
     return cleaned_str 
 
 
+def test(prediction, target, dataset=None, out=False):
+    """
+    Returns evaluation metrics
+    :param target: tensor containing target values
+    :param prediction: tensor containing prediction values
+    :param dataset: (optional, required to produce trace file) test dataset
+    :param out: (optional, required to produce trace file) whether to produce trace or not
+    :return: tuple containing precision, recall, and f1 values
+    """
+    t = np.array([x.item() for x in target])
+    p = np.array([pred(x) for x in prediction])
+    if out is not False:
+        if dataset is None:
+            raise ValueError("Dataset is needed to retrieve tweet ids!")
+        with open("eval_NB-BOW-" + id + ".txt", "w") as file:
+            for i in range(len(dataset)):
+                tweet_id = dataset[i][2]
+                prediction_text = "yes" if pred(prediction[i]) == 1 else "no"
+                prediction_proba = prediction[i][1].item() if prediction_text == "yes" else prediction[i][0].item()
+                target_text = "yes" if target[i] == 1 else "no"
+                outcome = "correct" if prediction_text == target_text else "wrong"
+                line = """{}  {}  {:.4}  {}  {}\n""".format(tweet_id, prediction_text, prediction_proba, target_text, outcome)
+                file.write(line)
+        print(f"Trace file produced: 'eval_NB-BOW-{id}.txt'")
+    try:
+        pre = precision_score(t, p)
+        rec = recall_score(t, p)
+        f1 = f1_score(t, p)
+    except ValueError:
+        f1 = pre = rec = 0
+    return pre, rec, f1
+
+
+def evaluateModel(model, prediction, target):
+    """
+    Produces output file containing model evaluation
+    :param model: lstm torch model
+    :param prediction: tensor containing prediction values
+    :param target: tensor containing target values
+    :return: None
+    """
+    t_yes = np.array([x.item() for x in target])
+    t_no = np.array([(lambda x: 1 if x == 0 else 0)(n) for n in t_yes])
+    p_yes = np.array([pred(x) for x in prediction])
+    p_no = np.array([(lambda x: 1 if x == 0 else 0)(n) for n in p_yes])
+    try:
+        pre_yes = precision_score(t_yes, p_yes)
+        rec_yes = recall_score(t_yes, p_yes)
+        f1_yes = f1_score(t_yes, p_yes)
+        acc = accuracy_score(t_yes, p_yes)
+
+        pre_no = precision_score(t_no, p_no)
+        rec_no = recall_score(t_no, p_no)
+        f1_no = f1_score(t_no, p_no)
+    except ValueError:
+        f1_yes = pre_yes = rec_yes = 0
+        f1_no = pre_no = rec_no = 0
+    with open("eval_NB-BOW-" + model.id + ".txt", "w") as file:
+        file.write("{:.4}\n".format(acc))
+        file.write("{:.4}  {:.4}\n".format(pre_yes, pre_no))
+        file.write("{:.4}  {:.4}\n".format(rec_yes, rec_no))
+        file.write("{:.4}  {:.4}\n".format(f1_yes, f1_no))
+    print(f"Evaluation file produced: 'eval_NB-BOW-{model.id}.txt'")
+
